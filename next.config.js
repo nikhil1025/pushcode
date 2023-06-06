893f6afc3071b4dbbf8819d40bb2c5f92553b23a
nikhilkrs1025 <nikhils@rede.net>
2023-06-06 23:16:31 +0530

    first commit-2

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ["images.unsplash.com"],
-    unoptimized: true
   },
-  output: "export"
 };
 
 module.exports = nextConfig;
